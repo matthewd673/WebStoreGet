9b8fe3bca2325b5e7b13e9989c302ed327f579ae
Matthew Daly <matthewd673@gmail.com>
2019-02-02 11:49:38 -0500

    Remove version input, fix typo

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
             <h3>Web Store Get</h3>
             <form action="get.php" method="get" target="_blank">
                 <input type="text" name="id" style="width: 300px;" placeholder="Extension ID"><br>
-                <input type="text" name="version" placeholder="Chrome Version"><br>
                 <input type="submit" value="Download CRX">
             </form>
             <p>Alternatively, you can make a GET request manually:<br>
@@ -19,11 +18,10 @@
             <br>
             <p><b>Brief Instructions</b><br>
             1. Find the ID of the extension on the Web Store (those letters at the end of the URL)<br>
-            2. Find your Chrome version at <span style="color: blue;">chrome://about</span><br>
-            3. Download the CRX file<br>
-            4. Open <span style="color: blue;">chrome://extensions</span> at drag in the CRX</p>
+            2. Download the CRX file<br>
+            3. Open <span style="color: blue;">chrome://extensions</span> and drag in the CRX</p>
             <br>
-            <p><i>Note: this tool is not intended to archive or otherwise store Web Store files long-term.</i></p>
+            <p><i>Note: this tool is not intended to archive or otherwise store extensions long-term.</i></p>
         </center>
     </body>
 </html>
